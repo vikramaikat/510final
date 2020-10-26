1b88e58464215919b201c23312fad43a64f2d9c7
Jack <12833246+jackgoffinet@users.noreply.github.com>
2020-10-25 22:59:18 -0400

    finished multiprocessing w/ standard backprop

--- parallel_script.py
+++ parallel_script.py
@@ -4,10 +4,12 @@ A simple example, now with paralllelism.
 TO DO
 -----
 - Make sure this is working correctly.
-- Wrap everything in torch.nn.Module
+- Add a loss plot.
+- Add batches, gpipe microbatches.
 """
 __date__ = "October 2020"
 
+from matplotlib.cm import get_cmap
 import matplotlib.pyplot as plt
 import multiprocessing as mp
 import numpy as np
@@ -16,23 +18,24 @@ import torch
 torch.autograd.set_detect_anomaly(True) # useful for debugging
 
 BATCH_DIM = 100
-VERBOSE = False
 
 
 class NoOp(torch.nn.Module):
 	"""Useful for caching gradients."""
 
-	def __init__(self, dim_1, dim_2):
+	def __init__(self, dim):
 		super(NoOp, self).__init__()
-		self.bias = torch.zeros(dim_1, dim_2, requires_grad=True)
+		self.dim = dim
+		self.bias = None
 
 	def forward(self, x):
+		self.bias = torch.zeros(x.shape[0], self.dim, requires_grad=True)
 		return self.bias + x
 
 	def zero(self):
-		"""Return to zeros."""
-		with torch.no_grad():
-			self.bias.data = torch.zeros_like(self.bias.data)
+		"""Return to None."""
+		self.bias = None
+
 
 
 def get_xor_training_data(n_samples=BATCH_DIM, seed=42, std_dev=0.5):
@@ -86,7 +89,7 @@ def make_dense_net(layer_dims, include_no_op=True, include_last_relu=True):
 	assert len(layer_dims) >= 2
 	layer_list = []
 	if include_no_op:
-		layer_list.append(NoOp(BATCH_DIM, layer_dims[0]))
+		layer_list.append(NoOp(layer_dims[0]))
 	for i in range(len(layer_dims)-1):
 		layer_list.append(torch.nn.Linear(layer_dims[i], layer_dims[i+1]))
 		if include_last_relu or i != len(layer_dims)-2:
@@ -94,100 +97,130 @@ def make_dense_net(layer_dims, include_no_op=True, include_last_relu=True):
 	return torch.nn.Sequential(*layer_list).to(torch.float)
 
 
-def mp_target_func(net_dims, parent_conn, child_conn, target_conn):
+def mp_target_func(net_dims, parent_conn, child_conn, final_layer):
 	"""
-	Spawn a torch.nn.Module
+	Spawn a torch.nn.Module and wait for data.
+
+	This is the meat of the parallelization.
 
 	Parameters
 	----------
 	net_dims : list of int
 		List of layer dimensions
 	parent_conn : multiprocessing.connection.Connection
-	child_conn : multiprocessing.connection.Connection or None
-	target_conn : multiprocessing.connection.Connection or None
+	child_conn : multiprocessing.connection.Connection
 	"""
-	if VERBOSE:
-		print("Process created with pid:", os.getpid(), "\n\tdims:", net_dims)
 	# Make a network.
-	net = make_dense_net(net_dims, include_last_relu=(target_conn is None))
+	net = make_dense_net(net_dims, include_last_relu=(not final_layer))
 	# Make an optimizer.
 	optimizer = torch.optim.Adam(net.parameters())
-
+	# Enter main loop.
 	epoch = 0
 	while True:
 		epoch += 1
 		# Receive data from our parent.
-		data = parent_conn.recv()
-
+		backwards_flag, data = parent_conn.recv()
 		# Return on None.
 		if data is None:
-			if child_conn is not None:
-				child_conn.send(None)
+			child_conn.send((None,None))
 			return
-
-		if VERBOSE:
-			print("process", os.getpid(), "received data:", data.shape, data.requires_grad)
-
 		# Zero gradients.
 		optimizer.zero_grad()
-
 		# Make a forward pass.
 		output = net(data)
-		if child_conn is None:
-			# If we're the last Module, calculate a loss.
-			target = target_conn.recv() # Receive the targets.
-			if VERBOSE:
-				print("process", os.getpid(), "received target:", target.shape)
-			loss = torch.mean(torch.pow(output - target, 2))
-			if epoch % 100 == 0:
-				print("epoch:", epoch ,"loss:", loss.item())
-			loss.backward()
-		else:
-			# Otherwise, pass the output to our children.
-			if VERBOSE:
-				print("process", os.getpid(), "sending data:", output.shape)
-			child_conn.send(output.detach())
-			grads = child_conn.recv()
-			if VERBOSE:
-				print("process", os.getpid(), "received grads:", grads.shape)
-			# Fake a loss with the correct gradients.
-			loss = torch.sum(output * grads)
-			loss.backward()
-			# output.backward(gradient=grads) # should do the same thing as above
-		# Pass gradients back.
-		parent_conn.send(net[0].bias.grad)
-		# Update this module's parameters.
-		optimizer.step()
-		# And zero out the NoOp.
-		net[0].zero()
+		if backwards_flag: # If we're going to do a backwards pass:
+			if final_layer:
+				# If we're the last Module, calculate a loss.
+				target = child_conn.recv() # Receive the targets.
+				loss = torch.mean(torch.pow(output - target, 2))
+				if epoch % 100 == 0:
+					print("epoch:", epoch ,"loss:", loss.item())
+				loss.backward()
+			else:
+				# Otherwise, pass the output to our children.
+				child_conn.send((True, output.detach()))
+				grads = child_conn.recv()
+				# Fake a loss with the correct gradients.
+				loss = torch.sum(output * grads)
+				loss.backward()
+				# output.backward(gradient=grads) # should do the same thing as above
+			# Pass gradients back.
+			parent_conn.send(net[0].bias.grad)
+			# Update this module's parameters.
+			optimizer.step()
+			# And zero out the NoOp layer.
+			net[0].zero()
+		else: # If we're just doing a forwards pass:
+			# Just feed the activations to the child.
+			child_conn.send((False, output.detach()))
 
 
-def training_run(model, epochs=1001, print_freq=100):
+
+class MPNet(torch.nn.Module):
 	"""
-	Train the model.
+	Network made up of distinct parameter partitions in different processes.
 
-	Parameters
-	----------
-	model : torch.nn.Module
-		Network to train
-	epochs : int, optional
-		Training epochs
-	print_freq : int, optional
+	Trained with standard backprop.
+
+	TO DO
+	-----
+	* test
+	* save/load models
 	"""
-	features, targets = get_xor_training_data()
-	optimizer = torch.optim.Adam(model.parameters())
-	for epoch in range(epochs):
-		optimizer.zero_grad()
-		prediction = model(features)
-		loss = torch.mean(torch.pow(prediction - targets, 2))
-		if epoch % print_freq == 0:
-			print("epoch", epoch, "loss", loss.item())
-		loss.backward()
-		optimizer.step()
+
+	def __init__(self, net_dims):
+		""" """
+		super(MPNet, self).__init__()
+		# Make all the pipes.
+		self.pipes = [mp.Pipe() for _ in range(len(net_dims)+1)]
+		# Make each network.
+		self.processes = []
+		for i, sub_net_dims in enumerate(net_dims):
+			conn_1 = self.pipes[i][1]
+			conn_2 = self.pipes[i+1][0]
+			final_layer = (i == len(net_dims)-1)
+			p = mp.Process( \
+					target=mp_target_func,
+					args=(sub_net_dims, conn_1, conn_2, final_layer),
+			)
+			self.processes.append(p)
+			p.start()
+
+
+	def forward(self, x):
+		"""Just the forward pass!"""
+		# Send features to the first parition.
+		self.pipes[0][0].send((False, x))
+		# Get predictions from last partition.
+		_, prediction = self.pipes[-1][1].recv()
+		return prediction
+
+
+	def forward_backward(self, x, y):
+		"""Both forward and backward passes."""
+		# Send features to the first parition.
+		self.pipes[0][0].send((True, x))
+		# Send targets to the last parition.
+		self.pipes[-1][1].send(y)
+		_ = self.pipes[0][0].recv() # Wait for gradients to come back.
+
+
+	def join(self):
+		""" """
+		# Send a kill signal.
+		self.pipes[0][0].send((None,None))
+		# Wait for it to come round.
+		temp = self.pipes[-1][1].recv()
+		assert temp == (None, None)
+		# Join everything.
+		for p in self.processes:
+			p.join()
+
+
 
 
 def plot_model_predictions(model, max_x=3, grid_points=40):
-	""" """
+	"""Plot the model predictions on the XOR dataset."""
 	# Make a grid of points.
 	x = torch.linspace(-max_x,max_x,grid_points).to(torch.float)
 	y = torch.linspace(-max_x,max_x,grid_points).to(torch.float)
@@ -197,17 +230,24 @@ def plot_model_predictions(model, max_x=3, grid_points=40):
 	with torch.no_grad():
 		prediction = model(grid).detach().cpu().numpy()
 	prediction = prediction.reshape(grid_points,grid_points)
+	vmin, vmax = np.min(prediction), np.max(prediction)
 	# Plot predictions.
 	plt.imshow(prediction, extent=[-max_x,max_x,-max_x,max_x], \
-		interpolation='bicubic')
+		interpolation='bicubic', cmap='viridis', vmin=vmin, vmax=vmax)
 	plt.colorbar()
 	# Show training data.
 	features, targets = get_xor_training_data()
 	features, targets = features.numpy(), targets.numpy().flatten()
 	idx_0 = np.argwhere(targets == 0)
 	idx_1 = np.argwhere(targets == 1)
-	plt.scatter(features[idx_0,0].flatten(), features[idx_0,1].flatten(), c='r')
-	plt.scatter(features[idx_1,0].flatten(), features[idx_1,1].flatten(), c='k')
+	cmap = get_cmap('viridis')
+	color_0, color_1 = (0.0-vmin)/(vmax - vmin), (1.0-vmin)/(vmax - vmin)
+	color_0, color_1 = np.array(cmap(color_0)), np.array(cmap(color_1))
+	color_0, color_1 = color_0.reshape(1,-1), color_1.reshape(1,-1)
+	plt.scatter(features[idx_0,0].flatten(), features[idx_0,1].flatten(), \
+			c=color_1, edgecolors='k')
+	plt.scatter(features[idx_1,0].flatten(), features[idx_1,1].flatten(), \
+			c=color_0, edgecolors='k')
 	plt.ylabel('Feature 1')
 	plt.xlabel('Feature 2')
 	plt.title('Network Predictions')
@@ -219,25 +259,16 @@ if __name__ == '__main__':
 	# Get training data.
 	features, targets = get_xor_training_data()
 	# Define network dimensions.
-	net_1_dims = [2,10,10]
-	net_2_dims = [10,10,1]
-	# Make the pipes.
-	conn_1r, conn_1s = mp.Pipe() # Parent to first module
-	conn_2r, conn_2s = mp.Pipe() # First module to second
-	conn_3r, conn_3s = mp.Pipe() # Second module to parent
-	# Spawn the processes.
-	p1 = mp.Process(target=mp_target_func, args=(net_1_dims, conn_1r, conn_2s, None))
-	p1.start()
-	p2 = mp.Process(target=mp_target_func, args=(net_2_dims, conn_2r, None, conn_3r))
-	p2.start()
+	net_dims = [[2,10,10], [10,10,1]]
+	# Make the model.
+	model = MPNet(net_dims)
 	# Send in the features.
 	for epoch in range(1000):
-		conn_1s.send(features)
-		conn_3s.send(features)
-		_ = conn_1s.recv() # Gradients come back.
-	conn_1s.send(None) # This is the kill signal.
-	p1.join()
-	p2.join()
+		model.forward_backward(features, targets)
+	# Plot.
+	plot_model_predictions(model)
+	# Clean up.
+	model.join()
 
 
 
